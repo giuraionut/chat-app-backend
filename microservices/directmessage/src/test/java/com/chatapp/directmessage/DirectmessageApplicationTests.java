036cb7956ca4b91b9ba4a96dcfe950766369a416
Giura Ionut-Emanuel <86114927+giuraionut@users.noreply.github.com>
2021-11-12 17:51:28 +0200

    Add DM service project

---
+++ microservices/directmessage/src/test/java/com/chatapp/directmessage/DirectmessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.directmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectmessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
